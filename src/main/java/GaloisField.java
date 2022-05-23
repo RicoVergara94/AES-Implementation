77490cb7db30e27d04e6ba8f8dc120961e61b095
Oscar Vergara <oscar.vergara1994@gmail.com>
2022-05-23 05:47:32 -0400

    added galois addition, subtraction, and multiplication

--- src/main/java/GaloisField.java
+++ src/main/java/GaloisField.java
@@ -1,2 +1,32 @@
 public class GaloisField {
+    public static Byte gAddition(Byte a, Byte b) {
+        return (byte) (a^b);
+    }
+    public static Byte gSubtraction(Byte a, Byte b) {
+        return (byte) (a^b);
+    }
+
+    public static Byte gMultiplication(Byte a, Byte b) {
+
+        byte p = 0;
+        for(int i = 0; i < 8; i++) { // run this loop 8 times
+            if(isKthBitSet(b.intValue(), 1)) { // check if low bit is set on b, if it is then p = p ^ a
+                 p = (byte)(a ^ p);
+            }
+            if(isKthBitSet((int)a,8)) {
+                int aXored = a^27;
+            }
+            else {
+                a = (byte)((int)a << 1);
+            }
+            b = (byte)((int)b >> 1);
+        }
+
+        return p;
+    }
+
+    public static boolean isKthBitSet(int n, int k) {
+        if((n & (1 << (k - 1))) > 0) return true;
+        else return false;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -29,7 +29,29 @@ public class Main {
         }
         return a;
     }
+    public static boolean isKthBitSet(int n, int k) {
+        if((n & (1 << (k - 1))) > 0) return true;
+        else return false;
+    }
 
+    public static Byte gMultiplication(Byte a, Byte b) {
+
+        byte p = 0;
+        for(int i = 0; i < 8; i++) { // run this loop 8 times
+            if(isKthBitSet(b.intValue(), 1)) { // check if low bit is set on b, if it is then p = p ^ a
+                p = (byte)(a ^ p);
+            }
+            if(isKthBitSet((int)a,8)) {
+                int aXored = a^27;
+            }
+            else {
+                a = (byte)((int)a << 1);
+            }
+            b = (byte)((int)b >> 1);
+        }
+
+        return p;
+    }
     public static void main(String[] args) {
         String s = "Hello World";
         int[] array = {1,2,3,4,5,6};
@@ -49,8 +71,10 @@ public class Main {
 
 //        Key key = new Key();
 //        System.out.println(key.getKey().getBlock());
-        for(int i = 0; i < newArray.length; i++) {
-            System.out.print(newArray[i] + " ");
-        }
+//        for(int i = 0; i < newArray.length; i++) {
+//            System.out.print(newArray[i] + " ");
+//        }
+        Byte result = gMultiplication((byte)7, (byte)3);
+        System.out.println((int)result);
     }
 }
